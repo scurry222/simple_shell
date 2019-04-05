df0d4bcb20f7d4f1df36edd3b16c1a04f6a2d7dd
lroudge <lauradrc@hotmail.fr>
2019-04-05 02:23:18 +0000

    Come back to older version of main1

--- main1.c
+++ main1.c
@@ -13,26 +13,26 @@ int main(void)
 {
     pid_t child_pid;
     int status;
-    size_t len = 256, i;
+    size_t len = 256;
+    int i;
     char *ch;
-    char **argv;
+    char *argv[] = {"/bin/ls", "-l", "/usr/", NULL};
 
 	ch = malloc(len + 1);
     	printf("This is the prompt ($) ");
 	while((i = getline(&ch, &len, stdin)) != EOF)
 	{
-    		**argv = {ch, NULL};
 		child_pid = fork(); //creation of the child process
     		if (child_pid == -1) //error
     		{
-        		perror("Error:");
+        		perror("Error");
         		return (1);
     		}
     		if (child_pid == 0) //success
     		{
 			if (access(ch, X_OK))
 			{
-				if (execve(ch, argv, NULL) == -1)
+				if (execve(argv[0], argv, NULL) == -1)
 				{
 					perror("Error:");
 					return(-1);
