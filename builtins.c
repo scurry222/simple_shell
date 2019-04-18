b4a558c9608e4a990c5f6e430bca648fa77bd10b
lroudge <lauradrc@hotmail.fr>
2019-04-18 06:32:31 +0000

    Remove unnecessary exit functionnality

--- builtins.c
+++ builtins.c
@@ -103,27 +103,24 @@ long int exit_handler(char **tokens)
 	int flag = 0, i;
 	long int num = 0;
 
-	if (_strcmp(tokens[0], "exit") == 0)
+	if (tokens[1] == NULL)
+		return (num);
+	for (i = 0; tokens[1][i]; i++)
 	{
-		if (tokens[1] == NULL)
-			return (num);
-		for (i = 0; tokens[1][i]; i++)
+		if ((tokens[1][i] >= '0' && tokens[1][i] <= '9') || tokens[1][0] == '+')
 		{
-			if ((tokens[1][i] >= '0' && tokens[1][i] <= '9') || tokens[1][0] == '+')
-			{
-				flag = 1;
-				if (tokens[1][i + 1] < '0' || tokens[1][i + 1] > '9')
-					break;
-			}
-			else
+			flag = 1;
+			if (tokens[1][i + 1] < '0' || tokens[1][i + 1] > '9')
 				break;
-			flag = 0;
 		}
-		if (flag == 1)
-		{
-			num = _atoi(tokens[1]);
-			return (num);
-		}
+		else
+			break;
+		flag = 0;
+	}
+	if (flag == 1)
+	{
+		num = _atoi(tokens[1]);
+		return (num);
 	}
 	return (-1);
 }
